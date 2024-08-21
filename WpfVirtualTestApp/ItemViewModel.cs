0781647ff6c0ca4ad11480cefb3b9ff9c5f3a9ed
Takehiro,Kiryu <mystlive@angework.co.jp>
2024-08-21 15:57:52 +0900

    Initial commit

---
+++ WpfVirtualTestApp/ItemViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfVirtualTestApp
+{
+    public class ItemViewModel : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _description;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                if (_description != value)
+                {
+                    _description = value;
+                    OnPropertyChanged(nameof(Description));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfVirtualTestApp/MainWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfVirtualTestApp
+{
+    public class MainWindowViewModel
+    {
+        public ObservableCollection<ItemViewModel> Items { get; set; }
+
+        public ICommand ChangeAllTextCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            Items = new ObservableCollection<ItemViewModel>();
+
+            // デモ用に大量のデータを生成
+            for (int i = 0; i < 1000; i++)
+            {
+                Items.Add(new ItemViewModel
+                {
+                    Name = $"Item {i + 1}",
+                    Description = $"Description for item {i + 1}"
+                });
+            }
+
+            ChangeAllTextCommand = new RelayCommand(ChangeAllText);
+        }
+
+        private void ChangeAllText()
+        {
+            var random = new Random();
+            foreach (var item in Items)
+            {
+                // ランダムな文字列や数値を生成して、アイテムに設定
+                item.Name = $"Changed Name {random.Next(1, 1000)}";
+                item.Description = $"Changed Description {Guid.NewGuid()}";
+            }
+        }
+    }
+
+}
